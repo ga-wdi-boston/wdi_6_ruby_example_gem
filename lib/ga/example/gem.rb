c8bcb160cd5c304a8969794c117efa72bc53bf1c
David Fisher <tibbon@gmail.com>
2013-11-25 19:15:44 -0500

    Removed files

--- lib/ga/example/gem.rb
+++
@@ -1,9 +0,0 @@
-require "ga/example/gem/version"
-
-module Ga
-  module Example
-    module Gem
-      # Your code goes here...
-    end
-  end
-end
--- lib/ga/example/gem/version.rb
+++
@@ -1,7 +0,0 @@
-module Ga
-  module Example
-    module Gem
-      VERSION = "0.0.1"
-    end
-  end
-end
